31fcb24c701acfb862a08a8d806e9cb186e923c5
beProsto <lavionperavion@gmail.com>
2022-09-10 17:05:53 +0200

    penuys

--- server.py
+++ server.py
@@ -3,5 +3,5 @@ import http.server
 import ssl
 
 httpd =  http.server.HTTPServer(('0.0.0.0', 8443), http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='/etc/letsencrypt/live/budexit.wroclaw.pl/fullchain.pem', keyfile='/etc/letsencrypt/live/budexit.wroclaw.pl/privkey.pem', server_side=True)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='/etc/letsencrypt/live/budexit.wroclaw.pl/fullchain.pem', keyfile='/etc/letsencrypt/live/budexit.wroclaw.pl/privkey.pem')
 httpd.serve_forever()
