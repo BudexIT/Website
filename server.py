bebec4c117c3bf2817f9705fd8947d1aeed8130f
beProsto <lavionperavion@gmail.com>
2022-09-10 17:07:38 +0200

    https

--- server.py
+++ server.py
@@ -2,6 +2,6 @@
 import http.server
 import ssl
 
-httpd =  http.server.HTTPServer(('0.0.0.0', 8443), http.server.SimpleHTTPRequestHandler)
+httpd =  http.server.HTTPServer(('0.0.0.0', 443), http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile='/etc/letsencrypt/live/budexit.wroclaw.pl/fullchain.pem', keyfile='/etc/letsencrypt/live/budexit.wroclaw.pl/privkey.pem')
 httpd.serve_forever()
