2fadf1ad5b4dc12b25979235ee6559bf4ffd39bc
beProsto <lavionperavion@gmail.com>
2022-09-27 22:49:16 +0200

    welp

--- server/manage.js
+++ server/manage.js
@@ -4,32 +4,50 @@ const dir = require("./back/fileserve");
 
 const clientList = new Map();
 
+async function getBufferData(req) {
+	const buffers = [];
+
+	for await (const chunk of req) {
+		buffers.push(chunk);
+	}
+
+	return Buffer.concat(buffers).toString();
+}
+
 module.exports = async (req, res) =>  {
 	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-	if(req.method == "POST") {  
-		const buffers = [];
-
-		for await (const chunk of req) {
-		buffers.push(chunk);
-		}
 	
-		const pass = Buffer.concat(buffers).toString();
+	if(req.method == "POST" && !clientList.get(ip)) {
+		const pass = await getBufferData(req);
 		
 		// Temporary - need to set up a login system with hashed passwords
 		if(pass == "uname=beProsto&psw=Easy1234") {
 			console.log("Logged in!");
-			clientList.set(ip, true);
+			clientList.set(ip, {didAnything: true});
 			
-			setTimeout(() => {
-				console.log("Automatically logged out!");
-				clientList.delete(ip);
-			}, 1000*60);
+			const logout = () => {
+				if(clientList.get(ip).didAnything) {
+					clientList.get(ip).didAnything = false;
+					setTimeout(logout, 1000*60);
+				}
+				else {
+					console.log("Automatically logged out!");
+					clientList.delete(ip);
+				}
+			};
+			setTimeout(logout, 1000*60);
 		}
+	}
+	else if(req.method == "POST" && clientList.get(ip)) {
+		const command = await getBufferData(req);
 		
+		console.log(command);
+
+		clientList.get(ip).didAnything = true;
 	}
 
-	if(!clientList.get(ip)) {
+
+	if(!clientList.get(ip)) {	
 
 		const {mime, data} = dir.loadFileData("protected/manage/login.html");
 
@@ -39,7 +57,9 @@ module.exports = async (req, res) =>  {
 
 	}
 	else {
-
+		
+		clientList.get(ip).didAnything = true;
+		
 		const {mime, data} = dir.loadFileData("protected/manage/logon.html");
 
 		res.setHeader("Content-Type", mime);
