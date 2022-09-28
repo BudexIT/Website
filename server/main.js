cbf6b60143fcf3fd300b97f9ea4785b60cc4d861
beProsto <lavionperavion@gmail.com>
2022-09-28 20:12:15 +0200

    inquirer

--- server/main.js
+++ server/main.js
@@ -1,1 +1,9 @@
+const fs = require("fs");
+
+const users = fs.readFileSync("private/users.json");
+if(!users) {
+	console.log("Please create a user:");
+	
+}
+
 const server = require("./back/server");
