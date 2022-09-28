f614937c4c18718403fd1d9d241014d3c68d557c
beProsto <lavionperavion@gmail.com>
2022-09-28 20:34:58 +0200

    module

--- server/main.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const users = fs.readFileSync("private/users.json");
-if(!users) {
-	console.log("Please create a user:");
-	
-}
-
-const server = require("./back/server");
---
+++ server/main.mjs
@@ -0,0 +1,41 @@
+import fs from "fs";
+import inquirer from "inquirer";
+
+try {
+	fs.readFileSync("private/users.json");
+	startApp();
+}
+catch {
+	console.log("Please create a user:");
+
+	const questions = [
+		{
+			type: 'input',
+			name: 'name',
+			message: "Username:",
+		},
+		{
+			type: 'password',
+			name: 'pass',
+			message: "Password:",
+		}
+	];
+		
+	inquirer.prompt(questions).then(answers => {
+		console.log(`User created!`);
+
+		const users = {};
+
+		users[answers.name] = answers.pass;
+
+		const text = JSON.stringify(users);
+
+		fs.writeFileSync("private/users.json", text);
+
+		startApp();
+	});
+}
+
+function startApp() {
+	import("./back/server.js");
+}
